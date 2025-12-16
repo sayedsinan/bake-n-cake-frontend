612abb7d2cd5da2a22f023662adce9a279ec7225
Sayed Sinan Umer Jifry <sinan.jifry@lifosys.com>
2025-12-16 21:49:08 +0530

    login page adding

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import styles from "./login.module.css";
 export default function Login() {
   return (
     <main className={styles.main}>
-      <div className={styles["login-box"]}
-      >
-        <h2 className={styles["heading"]}>Login</h2>
-        <h2 className={styles["heading-2"]}>Login</h2>
+      <div className={styles["heading-3"]}>Bake 'n Cake </div>
+      <div className={styles["login-box"]}>
+        <h2 className={styles["heading"]}>Welcome Back</h2>
+        <h2 className={styles["heading-2"]}>Sign in to your account to continue
+
+        </h2>
       </div>
     </main>
   );
